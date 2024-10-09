680d7d985b61e4beec7a0e42b23b32283daaecda
willyan miranda <willyanmiranda041@gmail.com>
2024-10-09 18:23:34 -0300

    add ticket component

--- src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, ElementRef, viewChild, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, EventEmitter, output, Output, viewChild, ViewChild } from '@angular/core';
 import { ButtonComponent } from '../../../shared/button/button.component';
 import { ControlComponent } from "../../../shared/control/control.component";
 import { FormsModule } from '@angular/forms';
@@ -13,12 +13,15 @@ import { FormsModule } from '@angular/forms';
 export class NewTicketComponent implements AfterViewInit {
   //@ViewChild('form') form?: ElementRef<HTMLFormElement>;
   private form = viewChild<ElementRef<HTMLFormElement>>('form');
+  //@Output() add = new EventEmitter()
+  add = output<{title: string; text: string}>();
 
   ngAfterViewInit() {
     console.log('after')
   }
 
   onSubmit(title: string, ticketText: string) {
+    this.add.emit({ title: title, text: ticketText });
     this.form()?.nativeElement.reset();
   }
 }
--- src/app/dashboard/tickets/ticket/ticket.component.ts
+++ src/app/dashboard/tickets/ticket/ticket.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, input, output, signal } from '@angular/core';
+import { Ticket } from '../ticket.model';
 
 @Component({
   selector: 'app-ticket',
@@ -8,5 +9,16 @@ import { Component } from '@angular/core';
   styleUrl: './ticket.component.css'
 })
 export class TicketComponent {
+  data = input.required<Ticket>();
+  close = output();
+  detailsVisible = signal(false);
 
+  onToggleDetails() {
+    this.detailsVisible.set(!this.detailsVisible);
+    //this.detailsVisible.update(wasVisible => !wasVisible)
+  }
+
+  onMarkAsCompleted() {
+    this.close.emit();
+  }
 }
